9bb7ee23e9129f2a3746f77b0eaeb59648ca50ff
hector036 <nayeem1505>
2019-11-27 21:08:42 +0600

    Test commit

---
+++ Quizzer- Online Quiz App/app/src/main/java/com/example/quizzer_onlinequizapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.quizzer_onlinequizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
