884a29912d01fa709a4a58c2df365986ff76bbfb
hector036 <nayeem1505>
2019-12-08 16:45:36 +0600

    Simple notification done.

--- Quizzer- Online Quiz App/app/src/main/java/com/example/quizzer_onlinequizapp/MainActivity.java
+++ Quizzer- Online Quiz App/app/src/main/java/com/example/quizzer_onlinequizapp/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.quizzer_onlinequizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button startButton,bookmarkBtn;
+    private Button startButton, bookmarkBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +35,37 @@ public class MainActivity extends AppCompatActivity {
 
         loadAds();
 
+
+        NotificationChannel channel = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel("TextNoti", "TextNoti", NotificationManager.IMPORTANCE_DEFAULT);
+        }
+
+        NotificationManager manager = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            manager = getSystemService(NotificationManager.class);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            manager.createNotificationChannel(channel);
+        }
+
+
+        FirebaseMessaging.getInstance().subscribeToTopic("textnoti")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Successfull";
+                        if (!task.isSuccessful()) {
+                            msg = "Failed";
+                        }
+                        //Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent categoryIntent = new Intent(MainActivity.this,CategoriesActivity.class);
+                Intent categoryIntent = new Intent(MainActivity.this, CategoriesActivity.class);
                 startActivity(categoryIntent);
             }
         });
@@ -38,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         bookmarkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent bookmarkIntent = new Intent(MainActivity.this,BookmarksActivity.class);
+                Intent bookmarkIntent = new Intent(MainActivity.this, BookmarksActivity.class);
                 startActivity(bookmarkIntent);
             }
         });
---
+++ Quizzer- Online Quiz App/app/src/main/java/com/example/quizzer_onlinequizapp/MessingServiceClass.java
@@ -0,0 +1,40 @@
+package com.example.quizzer_onlinequizapp;
+
+import android.app.Notification;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessingServiceClass extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+
+    public void showNotification(String title, String message){
+
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        long vibrate[]={100,600,100,600};
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"TextNoti")
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.icon_notification)
+                .setAutoCancel(true)
+                .setContentText(message)
+                .setVibrate(vibrate)
+                .setLights(Color.GREEN,1000,2000)
+                .setSound(soundUri);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(999,builder.build());
+    }
+}
